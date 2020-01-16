6d31f3413edfed52c3523129488ca76a095580e1
LG-STEM Gates <gatesinfo@gmail.com>
2020-01-16 05:59:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,9 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case "age >= 13 &&  age <= 19":
-      return "You are a teenager";
+    case "13, 14, 15, 16, 17, 18, 19":
+      return "You are a teenager"
     default:
-      return "You have an age";
+      return "You have an age"
  }
 }
